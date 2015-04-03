fdc23d20bd0e91fdeede953db1b7518bd81c41b1
shaihitdin <shaihitdin@gmail.com>
2015-04-03 22:27:55 +0600

    Create check.cpp

---
+++ check.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  freopen ("in", "r", stdin);
+  freopen ("out", "w", stdout);
+  int a, b;
+  cin >> a >> b;
+  cout << a + b;
+  return 0;
+}
