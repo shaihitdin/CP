25da34fa3995cc8a02bf0b7cc1b9b51b21f3df63
shaihitdin <shaihitdin@gmail.com>
2015-09-07 10:42:52 +0600

    SS
    SS

---
+++ Olymp/clean.bat
@@ -0,0 +1,17 @@
+@echo off
+set param=%1                
+
+del %param% /s in
+del %param% /s *.in
+del %param% /s out
+del %param% /s *.out
+del %param% /s err
+del %param% /s *.err
+del %param% /s input.txt
+del %param% /s output.txt
+del %param% /s *.exe
+
+
+for %%i in (a, b, c, d, e, f, g, h) do (
+	del %param% /s %%i.exe
+)
